c4cfda5a54d7b314a53d5dfd93be66bbb04098ec
yahmed137 <y.ahmed137@gmail.com>
2026-03-02 10:33:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1073,7 +1073,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 msg = (
                     f"رمز السهم: *{item['ticker'].split('.')[0]}*\n"
                     f"الاسم       : `{item['name']}`\n"
-                    f"الفاصل       : `{(tf_label)}`\n"
+                    f"الفاصل       : `{tf_label}`\n"
                     f"آخر إغلاق : `{item['last_close']}`\n"
                     f"قاع (5)    : `{item['entry']}`\n"
                     f"خط (1←4)  : `{item['target']}`\n"
