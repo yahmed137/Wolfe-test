0e0f63981f73f341e0bbd85dc134e058520832d8
yahmed137 <y.ahmed137@gmail.com>
2026-03-09 00:44:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1427,7 +1427,8 @@ def compute_score_criteria(d):
         ('7. خط الماكد > خط الإشارة', pd.notna(last['MACD']) and pd.notna(last['MACD_Sig']) and float(last['MACD']) > float(last['MACD_Sig'])),
         ('8. الماكد هستوجرام > 0', pd.notna(last['MACD_H']) and float(last['MACD_H']) > 0),
         ('9. مؤشر القوة النسبية > 50', pd.notna(last['RSI']) and float(last['RSI']) > 50),
-        ('10. ROC 12 إيجابي - .خم صاعد', pd.notna(last['ROC12']) and float(last['ROC12']) > 0),
+        #('10. ROC 12 إيجابي - زخم صاعد', pd.notna(last['ROC12']) and float(last['ROC12']) > 0),
+        ('10. ROC 12 \u200Eإيجابي - زخم صاعد\u200E', pd.notna(last['ROC12']) and float(last['ROC12']) > 0),
         ('11. مؤشر ADX > 25', pd.notna(last['ADX']) and float(last['ADX']) > 25),
         ('12. مؤشر +DI > مؤشر -DI', pd.notna(last['PDI']) and pd.notna(last['MDI']) and float(last['PDI']) > float(last['MDI'])),
     ]
